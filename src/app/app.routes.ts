8d657aaffa87c49c7faa7b207c78023e70dca739
Parimal <Parimal.c@quickheal.com>
2025-01-16 14:08:43 +0530

    routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppComponent }, // Redirect to HomeComponent
+    { path: '**', redirectTo: '', pathMatch: 'full' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule {}
